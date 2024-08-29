7c389a1f1b40f7d39a7464879544efd268c18720
Ceci <165979159+wushg1224@users.noreply.github.com>
2024-08-29 17:50:30 +1000

    subplot

--- Cecilia.py
+++ Cecilia.py
@@ -1,23 +1,27 @@
-#Automatic License/Number Plate Recognition (ANPR) 
-#using Python and OpenCV
-#using the Optical Character Recognition technique to identify the text on the plate.
+import cv2
+import matplotlib.pyplot as plt
 
-# import the necessary packages
-pip install easyocr
-pip install imutils
-pip install opencv-python
-pip install matplotlib
+# 读取图像
+img = cv2.imread("Test1.jpg")  # read image
 
-import cv2 
-from matplotlib import pyplot as plt
-import numpy as np
-import easyocr
-import imutils
-import random
+# 将图像转换为灰度图像
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert image to gray
 
+# 应用双边滤波以减少噪声
+bfilter = cv2.bilateralFilter(gray, 11, 17, 17)  # Noise reduction
 
+# 创建子图
+plt.figure(figsize=(10, 5))
 
+# 显示原始图像
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title('Original Image')
 
+# 显示处理后的图像
+plt.subplot(1, 2, 2)
+plt.imshow(bfilter, cmap='gray')
+plt.title('Processed Image')
 
-
-
+# 显示图形
+plt.show()
