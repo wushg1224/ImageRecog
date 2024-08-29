a7fbd2acef9387ac062cd4f944b8f0c237f13324
Ceci <165979159+wushg1224@users.noreply.github.com>
2024-08-29 15:46:11 +1000

    Create Cecilia.py

---
+++ Cecilia.py
@@ -0,0 +1,23 @@
+#Automatic License/Number Plate Recognition (ANPR) 
+#using Python and OpenCV
+#using the Optical Character Recognition technique to identify the text on the plate.
+
+# import the necessary packages
+pip install easyocr
+pip install imutils
+pip install opencv-python
+pip install matplotlib
+
+import cv2 
+from matplotlib import pyplot as plt
+import numpy as np
+import easyocr
+import imutils
+import random
+
+
+
+
+
+
+
